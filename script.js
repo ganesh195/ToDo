14f84187aef98d871f3e1ce2811eb7e4c0082264
Ganesh Sabat <77581432+ganesh195@users.noreply.github.com>
2023-02-08 23:35:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+document.getElementById("btn").addEventListener('click', 
+    function(){
+
+        let input = document.getElementById("input").value;
+
+        let elem=document.createElement('li');
+        elem.className='lists';
+        elem.id='list';
+        elem.innerText=input;
+
+        let btn=document.createElement('button');
+        btn.value='Done';
+        btn.innerText="Done";
+
+        let btn1=document.createElement('button');
+        btn1.value="Delete";
+        btn1.innerText="Delete";
+
+
+        let ul=document.querySelector('ul');
+
+        btn.style.border="none";
+        btn.style.marginLeft="3%";
+        btn.style.bordeRadius="8px";
+        btn.style.backgroundColor='#b4b3d8';
+        btn1.style.border="none";
+        btn1.style.marginLeft="3%";
+        btn1.style.bordeRadius="8px";
+        btn1.style.backgroundColor='#b4b3d8';
+
+        ul.appendChild(elem);
+        elem.appendChild(btn);
+        elem.appendChild(btn1);
+
+        btn.addEventListener('click',
+        function(){
+            elem.style.texDecorattion="line-through";
+        }
+        );
+        btn1.addEventListener('click',
+        function(){
+            elem.remove();
+        }
+        );
+
+        document.getElementById('input').value=""
+    }
+    );
